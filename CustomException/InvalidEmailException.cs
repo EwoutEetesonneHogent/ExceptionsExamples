2c149a6ca573287b3604352edd6c983b1b6bed75
Ewout Eetesonne <ewout.eetesonne@hogent.be>
2024-02-23 12:29:16 +0100

    Added custom exception example

---
+++ CustomException/InvalidEmailException.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomException
+{
+	internal class InvalidEmailException : ApplicationException
+	{
+		public InvalidEmailException(string userInput) : base($"Input entered was wrong: {userInput}")
+		{
+		_userInput = userInput;
+
+		}
+
+		private string _userInput;
+	}
+}
---
+++ CustomException/Program.cs
@@ -0,0 +1,14 @@
+﻿using CustomException;
+
+Console.WriteLine("Please input email");
+
+string userInput = Console.ReadLine();
+
+try
+{
+	if (!userInput.Contains("@"))
+	{
+		throw new InvalidEmailException(userInput);
+	}
+}
+catch (InvalidEmailException ex) { Console.WriteLine(ex.Message); }
