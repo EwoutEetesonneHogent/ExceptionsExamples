5088dca5568364fd4a2fe4b45c539244c92a029f
Ewout Eetesonne <ewout.eetesonne@hogent.be>
2024-02-23 10:13:43 +0100

    Added multiple catches

--- MultipleCatches/Program.cs
+++ MultipleCatches/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Please enter a file path");
+
+try
+{
+	Console.WriteLine( File.ReadAllText(Console.ReadLine()));
+}
+catch (EndOfStreamException)
+{
+	Console.WriteLine("File was not found, please try again");
+}
+catch (NullReferenceException ex)
+{
+	Console.WriteLine(ex.Message);
+}
+
+Console.WriteLine("Success!");
